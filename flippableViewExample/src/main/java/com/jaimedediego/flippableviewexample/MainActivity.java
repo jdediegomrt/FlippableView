a413b50bf56dc40acac730811eb1f7135b1a0050
Jaime <dediegomrt@gmail.com>
2019-02-22 08:35:24 +0100

    dev - add example app

---
+++ flippableViewExample/src/main/java/com/jaimedediego/flippableviewexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.jaimedediego.flippableviewexample;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.jaimedediego.flippableview.FlippableView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final FlippableView flippableView = findViewById(R.id.flippable_view);
+
+        flippableView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flippableView.flip();
+            }
+        });
+    }
+}
